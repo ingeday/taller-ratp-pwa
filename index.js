93163ddeb4e70e9cc70df409f9df35c6f04fda3c
Eday Gonzalez <edaygonzalez@ingenieros.com>
2020-08-23 19:30:27 -0500

    Agregamos paquete express-http-to-https

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require("express");
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
 var app = express();
+
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
 
 app.use(express.static('public'));
 
